4a66fd94201be5351f2167d9798e64ec87389639
dotnotation <dotinmotionproductions@gmail.com>
2021-07-30 18:20:54 -0400

    commit tree 3d085dd7897db226cc1a2750f73e791d3bd07da9

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,25 +12,29 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/registrations/signup' do
-
+    #render the sign-up form
     erb :'/registrations/signup'
   end
 
   post '/registrations' do
+    #get the new user info from params hash, creates a new user, signs them in, and redirects them
     @user = User.new(name: params["name"], email: params["email"], password: params["password"])
     @user.save
     session[:user_id] = @user.id
-
+    puts params
     redirect '/users/home'
   end
 
   get '/sessions/login' do
-
+    #rendering the login form
     # the line of code below render the view page in app/views/sessions/login.erb
     erb :'sessions/login'
   end
 
   post '/sessions' do
+    #receives the post request when a user hits submit on login form
+    #grabs user info from params hash, looks to match info to existing entries in the database
+    #if matching entry in found, signs the user in
     @user = User.find_by(email: params[:email], password: params[:password])
     if @user
       session[:user_id] = @user.id
@@ -40,12 +44,13 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/sessions/logout' do
+    #logs out user by clearing the session hash
     session.clear
     redirect '/'
   end
 
   get '/users/home' do
-
+    #renders user's homepage
     @user = User.find(session[:user_id])
     erb :'/users/home'
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ActiveRecord::Base
   validates_presence_of :name, :email, :password
+  #so no one can sign up without inputting all three
 end
